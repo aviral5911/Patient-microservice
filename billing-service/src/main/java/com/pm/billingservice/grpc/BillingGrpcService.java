62beb818571f56a454f19aaf2c9fe39ee0ef652e
aviral choudhary <aviralchaudhary95@gmail.com>
2026-06-18 22:43:14 +0530

    create billing service microservice and add grpc setup

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,31 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<billing.BillingResponse> responseObserver){
+
+        //business login calling service then repo layers
+
+
+        System.out.println("GRPC METHOD HIT");
+
+        billing.BillingResponse response = billing.BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+
+}
